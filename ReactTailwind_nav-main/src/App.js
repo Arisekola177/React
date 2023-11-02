9a20762d0464e2b5cb05be4d86df5d0a93392985
Azeez <shuaibazeez14@gmail.com>
2023-11-03 00:09:03 +0100

    first commit

---
+++ ReactTailwind_nav-main/src/App.js
@@ -0,0 +1,17 @@
+import Nav from './components/Nav'
+
+const App = () =>
+{
+  return (
+    <>
+      <Nav />
+      <div className="bg-indigo-600 w-full h-screen">
+           <div className='w-10/12 mx-auto bg-white'>
+               <h1>Aztech</h1>
+           </div>
+      </div>
+    </>
+  )
+}
+
+export default App
